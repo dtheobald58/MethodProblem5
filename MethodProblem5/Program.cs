e38e779205e6839b0ee370b899cdfbaefb00b781
dtheobald58 <dtheobald58@yahoo.com>
2016-09-15 15:33:20 -0400

    initial commit

---
+++ MethodProblem5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodProblem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to be doubled:");
+            
+            //using method "Double"
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("= "+Double(num));
+        }
+
+
+        //method to double an int
+        static int Double(int number)
+        {
+            int doubled = number * 2;
+            return doubled;
+        }
+    }
+}
